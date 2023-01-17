f9a0873a37464665dee444e2397847b690596570
D4riu5 <R3iji94@gmail.com>
2023-01-17 15:06:36 +0100

    Milestone 1 and 2 completed

--- js/script.js
+++ js/script.js
@@ -35,17 +35,69 @@ const rightArrow = document.getElementById(`righty`);
 const upArrow = document.getElementById(`uppy`);
 const downArrow = document.getElementById(`downy`);
 
+
 // counters 
+let selected = 0;
 
 // cycles
 for (let image of images){
-  carouselDiv.innerHTML += `<div class="slide"><img src="${image.image}"></div>`;
+  carouselDiv.innerHTML += `
+  <div class="slide">
+    <img src="${image.image}">
+    <div id="text-container" class="w-100">
+      <h4>${image.title}</h4>
+      <p class="w-75">${image.text}</p>
+    </div>  
+  </div>
+    `;
+    
   thumbnailsContainer.innerHTML += `<div class="thumbnail_slide"><img class="img2" src="${image.image}"></div>`;
 }
+
+
+const allSlides = document.querySelectorAll('.slide');
+const allThumbnails = document.querySelectorAll('.thumbnail_slide');
+
+console.log("All slides: " + allSlides);
+console.log("All thumbnail slides: " +allThumbnails);
+
+// added selected class to position 0
+allSlides[selected].classList.add('selected');
+allThumbnails[selected].classList.add("selected-thumbnail");
 
 
 // functions (independent)
 
 // functions (dependent)
+function next(){
+  if(selected == allThumbnails.length -1){
+      allSlides[selected].classList.remove('selected');
+      allThumbnails[selected].classList.remove('selected-thumbnail');
+      selected = 0;
+      allSlides[selected].classList.add('selected');
+      allThumbnails[selected].classList.add('selected-thumbnail');
+  } else{
+  allSlides[selected].classList.remove('selected');
+  allThumbnails[selected].classList.remove('selected-thumbnail');
+  selected++
+  allSlides[selected].classList.add('selected');
+  allThumbnails[selected].classList.add('selected-thumbnail');
+  }
+};
 
+function prev(){
+  if(selected == 0){
+      allSlides[selected].classList.remove('selected');
+      allThumbnails[selected].classList.remove('selected-thumbnail');
+      selected = allThumbnails.length -1;
+      allSlides[selected].classList.add('selected');
+      allThumbnails[selected].classList.add('selected-thumbnail');
+  } else {
+  allSlides[selected].classList.remove('selected');
+  allThumbnails[selected].classList.remove('selected-thumbnail');
+  selected--;
+  allSlides[selected].classList.add('selected');
+  allThumbnails[selected].classList.add('selected-thumbnail');
+  }
+};
 // events
